fbc20fd66657c065a97771c7137406abcdbcab46
AnatoliyKyznichenko <antonkorodan@gmail.com>
2024-04-09 15:52:20 +0300

    first commit main

--- backend/delivery/products/models.py
+++ backend/delivery/products/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Product_MY(models.Model):
+    name=models.Model(models.CharField(max_length=150))
+    def __str__(self):
+        return self.name
