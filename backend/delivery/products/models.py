669c758d61b6b23adbdca81d5926b8e419efda37
AnatoliyKyznichenko <antonkorodan@gmail.com>
2024-04-10 02:47:04 +0300

    com out

--- backend/delivery/products/models.py
+++ backend/delivery/products/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class MyProduct(models.Model):
+    name = models.CharField(max_length=155)
+
+    def __str__(self):
+        return self.name
